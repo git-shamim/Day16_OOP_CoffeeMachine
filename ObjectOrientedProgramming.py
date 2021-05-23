83ece5085e2d527e7caccd5394302612b584ca63
shamim-simplilearn <shamim.gcloud@gmail.com>
2021-05-23 18:31:16 +0530

    first commit

---
+++ ObjectOrientedProgramming.py
@@ -0,0 +1,51 @@
+# pypi
+
+# TODO : Class Turtle
+
+# from turtle import Turtle, Screen             # turtle is the module; Turtle and Screen are classes
+#
+# timmy = Turtle()                              # timmy is an object created from class Turtle
+# print(timmy)
+# timmy.shape("turtle")                         # shape is a method(function otherwise) of object timmy
+# timmy.color("coral")                          # color is a method(function otherwise) of object timmy
+# timmy.forward(100)
+#
+# my_screen = Screen()                          # my_screen is an object created from class Screen
+# print(my_screen.canvheight)                   # canvheight is an attribute(variable otherwise) of object my_screen
+# my_screen.exitonclick()                       # exitonclick is a method(function otherwise) of object my_screen
+
+
+# TODO : Class PrettyTable
+#
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmader"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+# table.align = 'l'
+# print(table)
+
+
+# TODO : creation of class, its attributes and methods
+
+class User:
+    
+    def __init__(self, id, name):
+        self.user_id = id
+        self.user_name = name
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, any_user):
+        self.following += 1
+        any_user.followers += 1
+
+
+user1 = User('001', 'john')
+user2 = User('002', 'jack')
+
+user1.follow(user2)
+
+print(user1.followers, user1.following)
+print(user2.followers, user2.following)
+
+
